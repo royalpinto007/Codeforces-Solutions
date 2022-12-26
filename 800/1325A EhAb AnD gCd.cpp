f2da895a921938cbcd0e9c4692fbfced30741f32
Royal Simpson Pinto <royalpinto007@gmail.com>
2022-12-26 20:14:06 +0530

    Add files via upload

---
+++ 800/1325A EhAb AnD gCd.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+# define fast ios_base :: sync_with_stdio (false) ; cin.tie(0) ; cout.tie(0)
+
+void solve ()
+{
+	int x ;
+	cin >> x ;
+	cout << 1 << ' ' << x-1 << '\n' ;
+}
+
+int main()
+{
+	fast ;
+	int t = 1 ;
+	cin>>t ;
+	while (t--)
+	  solve () ;
+	return 0 ;
+}
