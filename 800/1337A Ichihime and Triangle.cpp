b69b325068a7c07bf2feb400ed94d7a0f54db8a6
Royal Simpson Pinto <royalpinto007@gmail.com>
2023-01-05 17:42:12 +0530

    Add files via upload

---
+++ 800/1337A Ichihime and Triangle.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+#define FAST ios_base :: sync_with_stdio (false) ; cin.tie(0) ; cout.tie(0)
+
+void solve (int test_case)
+{
+	int a , b , c , d ;
+	cin >> a >> b >> c >> d ;
+	cout << b << ' ' << c << ' ' << c << '\n' ;
+}
+
+int main()
+{
+
+	FAST ;
+	int tc = 1 ;
+	cin >> tc ;
+	while (tc--)
+	    solve (tc) ;
+	return 0 ;
+}
